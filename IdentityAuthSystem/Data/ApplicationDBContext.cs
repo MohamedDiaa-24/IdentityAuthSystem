9232590abdbdbc80777709513ec652299e576aa7
Mohamed Diaa <mohamed.diaa.elsayed0@gmail.com>
2025-05-25 16:21:16 +0300

    Add users tables and First Migration

---
+++ IdentityAuthSystem/Data/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using IdentityAuthSystem.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+namespace IdentityAuthSystem.Data
+{
+    public class ApplicationDBContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
--- IdentityAuthSystem/Program.cs
+++ IdentityAuthSystem/Program.cs
@@ -1,8 +1,31 @@
+using IdentityAuthSystem.Data;
+using IdentityAuthSystem.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<ApplicationDBContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+{
+    options.Password.RequiredLength = 8;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireLowercase = false;
+    options.User.RequireUniqueEmail = true;
+    options.SignIn.RequireConfirmedEmail = false;
+    options.SignIn.RequireConfirmedAccount = false;
+    options.SignIn.RequireConfirmedPhoneNumber = false;
+
+}).AddEntityFrameworkStores<ApplicationDBContext>()
+.AddDefaultTokenProviders();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
