3d8da64388b983bf3be40f2ea81a731ec6622331
Mohamed Diaa <mohamed.diaa.elsayed0@gmail.com>
2025-05-27 23:35:29 +0300

    Create Login and Register Ations

--- IdentityAuthSystem/Controllers/AccountController.cs
+++ IdentityAuthSystem/Controllers/AccountController.cs
@@ -1,22 +1,88 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IdentityAuthSystem.Models;
+using IdentityAuthSystem.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityAuthSystem.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountController(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
         public IActionResult Login()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Email or Password is incorrect");
+                    return View();
+                }
+            }
+            return View();
+        }
+
+
         public IActionResult Register()
         {
             return View();
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ApplicationUser user = new ApplicationUser
+                {
+                    FullName = model.Name,
+                    UserName = model.Email,
+                    Email = model.Email,
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login", "Account");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                    return View(model);
+                }
+            }
+
+            return View(model);
+        }
         public IActionResult VerifyEmail()
         {
             return View();
         }
+
+
         public IActionResult ChangePassword()
         {
             return View();
